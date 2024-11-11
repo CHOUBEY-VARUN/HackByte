d7039da3df40b8b0ba304f9e9de947589c6bbc52
CHOUBEY-VARUN <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2024-11-11 19:54:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMarketplace() {
+    alert("Welcome to the Marketplace! Here you can buy, sell, or repair electronics.");
+}
+
+function showRecyclingCenters() {
+    alert("Find your nearest recycling centers by entering your location on our platform.");
+}
